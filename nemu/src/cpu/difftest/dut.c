bfa3dd50bc6fda28540c558d105a5c2210f2a351
WB2114024-Fang Guoxiang <1498872939@qq.com>
2024-04-06 19:02:47 +0800

    实现difftest

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -25,6 +25,7 @@ void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) =
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
+void inst_print();
 
 #ifdef CONFIG_DIFFTEST
 
@@ -96,6 +97,7 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
     isa_reg_display();
+    inst_print();
   }
 }
 
