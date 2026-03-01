c1a928034c7f5f3dda65fd42c748348710c7f2c3
fgx <1498872939@qq.com>
2026-03-02 02:53:30 +0800

    实现timer，优化printf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,17 +5,13 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int int_to_str(char *out, int num) {
+int int_to_str(char *out, int num, int width, bool fill_zero) {
   int n = 0;
+  int ori_num = num;
 
   if (num < 0) {
     out[n++] = '-';
     num = -num;
-  }
-
-  if (num == 0) {
-    out[n++] = '0';
-    return n;
   }
 
   char tmp[11];
@@ -24,6 +20,24 @@ int int_to_str(char *out, int num) {
     tmp[i++] = '0' + num % 10;
     num /= 10;
   }
+
+  if (i == 0) {
+    tmp[i++] = '0';
+  }
+
+  if (width != -1) {
+    int num_width = ori_num < 0? i + 1: i;
+    while (num_width < width) {
+      if (fill_zero == true) {
+        out[n++] = '0';
+      } else {
+        out[n++] = ' ';
+      }
+      num_width++;
+    }
+  }
+
+  
 
   while (i > 0) {
     i--;
@@ -34,7 +48,18 @@ int int_to_str(char *out, int num) {
 }
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  char buf[1024];
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsnprintf(buf, sizeof(buf), fmt, ap);
+  
+  for (int i = 0; i < ret; i++) {
+    putch(buf[i]);
+  }
+
+  va_end(ap);
+
+  return ret;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -65,6 +90,19 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
       out[len++] = fmt[p++];
     } else {
       p++;
+      bool fill_zero = false;
+      int width = -1;
+      if (fmt[p] == '0') {
+        fill_zero = true;
+        width = 0;
+        p++;
+      }
+
+      while (fmt[p] >= '0' && fmt[p] <= '9') {
+        width = width * 10 + fmt[p] - '0';
+        p++;
+      }
+
       if (fmt[p] == 's') {
         char *s = va_arg(ap, char *);
         while (*s != '\0') {
@@ -73,7 +111,7 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
         p++;
       } else if (fmt[p] == 'd') {
         int num = va_arg(ap, int);
-        len += int_to_str(out + len, num);
+        len += int_to_str(out + len, num, width, fill_zero);
         p++;
       } else {
         panic("Not implemented");
