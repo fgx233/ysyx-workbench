565e50fa332c384fc17651a35d7a3a346913c2f0
Fang guoxiang <1498872939@qq.com>
2026-06-22 17:44:29 +0800

    纯cpu逻辑，排除rom

--- npc/src/pc.scala
+++ npc/src/pc.scala
@@ -1,16 +1,14 @@
 import chisel3._
 
 class pc extends Module {
-  val pc_en   = IO(Input(Bool()))
   val next_pc = IO(Input(UInt(4.W)))//写pc地址数据
   val curr_pc = IO(Output(UInt(4.W)))//pc寄存器存储的地址
 
   //pc寄存器
   val pc_reg = RegInit(0.U(4.W))
 
-  when (pc_en) {
-    pc_reg := next_pc
-  } 
+
+  pc_reg := next_pc
   
   curr_pc := pc_reg
 }
--- npc/src/top.scala
+++ npc/src/top.scala
@@ -6,40 +6,25 @@ import chisel3.util._
   * register x is then the GCD
   */
 class top extends Module {
-  val button     = IO(Input(Bool()))
-
-  val seg_out1   = IO(Output(UInt(7.W)))
-  val seg_out2   = IO(Output(UInt(7.W)))
-  val seg_out3   = IO(Output(UInt(7.W)))
-
-  val seg_out4   = IO(Output(UInt(7.W)))
-  val seg_out5   = IO(Output(UInt(7.W)))
-  val seg_out6   = IO(Output(UInt(7.W)))
+  val inst      = IO(Input(UInt(8.W)))
+  val inst_addr = IO(Output(UInt(4.W)))
 
   val pc        = Module(new pc)
   val regs      = Module(new regs)
-  val rom       = Module(new rom)
-  //数码管寄存器  
-  val seg       = RegInit(0.U(8.W))
-
-  val button_delay = Reg(UInt(3.W))
-  button_delay := Cat(button_delay(1,0), button)
-  val flag      = !button_delay(2) & button_delay(1)
-  
 
 
   //指令译码标志信号
-  val is_add    = rom.inst(7,6) === "b00".U(2.W)
-  val is_out    = rom.inst(7,6) === "b01".U(2.W)
-  val is_li     = rom.inst(7,6) === "b10".U(2.W)
-  val is_bner0  = rom.inst(7,6) === "b11".U(2.W)
+  val is_add    = inst(7,6) === "b00".U(2.W)
+  val is_out    = inst(7,6) === "b01".U(2.W)
+  val is_li     = inst(7,6) === "b10".U(2.W)
+  val is_bner0  = inst(7,6) === "b11".U(2.W)
 
   //指令信息提取
-  val addr      = rom.inst(5,2)
-  val imm       = rom.inst(3,0)
-  val rs1       = rom.inst(3,2)
-  val rs2       = rom.inst(1,0)
-  val rd        = rom.inst(5,4)
+  val addr      = inst(5,2)
+  val imm       = inst(3,0)
+  val rs1       = inst(3,2)
+  val rs2       = inst(1,0)
+  val rd        = inst(5,4)
 
   
   //寄存器数据
@@ -52,31 +37,14 @@ class top extends Module {
   //regs输入连接
   regs.raddr1   := Mux(is_bner0, 0.U, rs1)
   regs.raddr2   := rs2  
-  regs.wen      := (is_add | is_li) & flag
+  regs.wen      := (is_add | is_li)
   regs.waddr    := rd
   regs.wdata    := rd_wdata
 
   //pc输入连接
-  pc.pc_en      := flag
   pc.next_pc    := pc_wdata
 
   //rom输入连接
-  rom.inst_addr := pc.curr_pc
-
-  when (is_out) {
-    seg := src1
-  }
-
-  val seg_bin_to_bcd = Module(new bcd)
-  seg_bin_to_bcd.bin := seg
-  seg_out1 := seg_bin_to_bcd.seg1
-  seg_out2 := seg_bin_to_bcd.seg2
-  seg_out3 := seg_bin_to_bcd.seg3
-
-  val seg_bin_to_bcd_pc = Module(new bcd)
-  seg_bin_to_bcd_pc.bin := Cat(0.U(4.W), pc.curr_pc)
-  seg_out4 := seg_bin_to_bcd_pc.seg1
-  seg_out5 := seg_bin_to_bcd_pc.seg2
-  seg_out6 := seg_bin_to_bcd_pc.seg3
+  inst_addr := pc.curr_pc
 
 }
