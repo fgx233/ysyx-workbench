91cf331c1b8fd684d6473351f269014695becb27
Fang guoxiang <1498872939@qq.com>
2026-06-22 15:36:00 +0800

    实现scpu

--- npc/src/top.scala
+++ npc/src/top.scala
@@ -1,66 +1,63 @@
 import chisel3._
 import chisel3.util.experimental.loadMemoryFromFile
+import chisel3.util.MuxLookup
 
 /** Compute GCD using subtraction method. Subtracts the smaller from the larger until register y is zero. value in
   * register x is then the GCD
   */
 class top extends Module {
-  val hsync = IO(Output(Bool()))
-  val vsync = IO(Output(Bool()))
-  val vga_ena = IO(Output(Bool()))
+  val seg_out1   = IO(Output(UInt(7.W)))
+  val seg_out2   = IO(Output(UInt(7.W)))
+  val seg_out3   = IO(Output(UInt(7.W)))
 
-  val R = IO(Output(UInt(8.W)))
-  val G = IO(Output(UInt(8.W)))
-  val B = IO(Output(UInt(8.W)))
+  val pc        = Module(new pc)
+  val regs      = Module(new regs)
+  val rom       = Module(new rom)
+  //数码管寄存器  
+  val seg       = RegInit(0.U(8.W))
 
+  //指令译码标志信号
+  val is_add    = rom.inst(7,6) === "b00".U(2.W)
+  val is_out    = rom.inst(7,6) === "b01".U(2.W)
+  val is_li     = rom.inst(7,6) === "b10".U(2.W)
+  val is_bner0  = rom.inst(7,6) === "b11".U(2.W)
 
-  val rom = Mem(307200, UInt(32.W))
-  // loadMemoryFromFile(rom, "/home/fgx/projects/Verilog/digital_expriment/vga/nvboard/resource/taki.hex")
+  //指令信息提取
+  val addr      = rom.inst(5,2)
+  val imm       = rom.inst(3,0)
+  val rs1       = rom.inst(3,2)
+  val rs2       = rom.inst(1,0)
+  val rd        = rom.inst(5,4)
 
-  val hcnt = RegInit(0.U(10.W))
-  val vcnt = RegInit(0.U(10.W))
-
-  when(hcnt === 799.U) {
-    hcnt := 0.U
-  }.otherwise {
-    hcnt := hcnt + 1.U
-  }
-
-  when(vcnt === 524.U && hcnt === 799.U) {
-    vcnt := 0.U
-  }.elsewhen(hcnt === 799.U){
-    vcnt := vcnt + 1.U
-  }
-
-  val hactive = (hcnt >= 144.U) && (hcnt <= 783.U)
-  val vactive = (vcnt >= 35.U) && (vcnt <= 514.U)
-
-
-  hsync := hcnt >= 96.U
-  vsync := vcnt >= 2.U
-  vga_ena := hactive && vactive
   
-  val x = Wire(UInt(10.W))
-  val y = Wire(UInt(10.W))
+  //寄存器数据
+  val src1      = regs.rdata1
+  val src2      = regs.rdata2
 
-  when(vga_ena) {
-    x := hcnt - 144.U
-    y := vcnt - 35.U
-  }.otherwise {
-    x := 0.U
-    y := 0.U
+  val rd_wdata  = Mux(is_add, src1 + src2, imm)
+  val pc_wdata  = Mux(is_bner0 && (src1 =/= src2), addr, pc.curr_pc + 1.U)
+
+  //regs输入连接
+  regs.raddr1   := Mux(is_bner0, 0.U, rs1)
+  regs.raddr2   := rs2  
+  regs.wen      := is_add | is_li
+  regs.waddr    := rd
+  regs.wdata    := rd_wdata
+
+  //pc输入连接
+  pc.next_pc    := pc_wdata
+
+  //rom输入连接
+  rom.inst_addr := pc.curr_pc
+
+  when (is_out) {
+    seg := src1
   }
 
-  val pixel = rom.read(x + y*640.U)
+  val seg_bin_to_bcd = Module(new bcd)
+  seg_bin_to_bcd.bin := seg
+  seg_out1 := seg_bin_to_bcd.seg1
+  seg_out2 := seg_bin_to_bcd.seg2
+  seg_out3 := seg_bin_to_bcd.seg3
 
-  when(vga_ena) {
-    R := pixel(23, 16)
-    G := pixel(15, 8)
-    B := pixel(7, 0)
-  }.otherwise {
-    R := 0.U
-    G := 0.U
-    B := 0.U
-  }
-  
 }
--- npc/verification/src/sim_main.cpp
+++ npc/verification/src/sim_main.cpp
@@ -10,64 +10,42 @@
 #define BITS(x, hi, lo) (((x) >> (lo)) & BITMASK((hi) - (lo) + 1)) // similar to x[hi:lo] in verilog
 #define SEXT(x, len) ({ struct { int64_t n : len; } __x = { .n = x }; (uint64_t)__x.n; })
 
-typedef struct info {
-  uint8_t input;
-  uint8_t output;
-  uint8_t flag;
-} info;
+VerilatedContext* contextp = nullptr;
+VerilatedFstC *tfp = nullptr;
+Vtop* top = nullptr;
 
-void encoder(info& d) {
-  uint8_t out = 0;
-  for (uint8_t i = 7; i > 0; i --) {
-    if ((d.input >> i) & 1) {
-      out = i;
-      break;
-    }
+void tick() {
+  top->clock = 0; top->eval(); tfp->dump(contextp->time()); contextp->timeInc(1);
+  top->clock = 1; top->eval(); tfp->dump(contextp->time()); contextp->timeInc(1);
+}
+
+void reset(int i) {
+  top->reset = 1;
+  while (i-- > 0)
+  {
+    tick();
   }
-  d.output = out;
-  d.flag = d.input != 0;
+  top->reset = 0;
 }
 
 int main(int argc, char** argv) {
   // 仿真上下文：管理仿真时间、命令行参数等
-  VerilatedContext* contextp = new VerilatedContext;
+  contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
   contextp->traceEverOn(true);
-  VerilatedFstC *tfp = new VerilatedFstC;
+  tfp = new VerilatedFstC;
   // 实例化被测模块
-  Vtop* top = new Vtop{contextp};
+  top = new Vtop{contextp};
 
   top->trace(tfp, 99);
   tfp->open("build/wave.fst");
+  
+  reset(10);
 
-  int total = 0;
-  int right = 0;
-  for (uint8_t i = 0; i <= 255; i ++) {
-
-    total ++;
-    top->io_in = i;
-    info d;
-    d.input = i;
-    encoder(d);
-
-    top->eval();
-    tfp->dump(contextp->time());
-    contextp->timeInc(1);
-
-    if (d.output == top->io_out && d.flag == top->io_flag) {
-      right ++;
-    } else {
-      std::cout << "输入：" << std::bitset<8>(i) << "参考模型 ：out = " << (int)d.output << " flag = " << (int)d.flag <<
-      "电路： out = " << (int)top->io_out << " flag = " << (int)top->io_flag << std::endl;
-    }
-
-    if (i == 255) {
-      break;
-    }
+  for (int i = 0; i < 100; i ++) {
+    tick();
   }
-
-  printf("总测试数%d次，正确%d次，错误%d次，正确率%f\n", total, right, total-right, (float)right/total);
-
+  
   // 收尾：结束仿真并释放资源
   top->final();
   tfp->close();
